cd26e77a5cd47b306c8f7707322f2234249e117b
yuwek <yuuuwek@gmail.com>
2023-12-24 20:32:46 -0300

    feat(weather): add error handling to `geolocation`

---
+++ apps/weather/src/components/ErrorCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+type Props = {
+  code: number
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="mt-6 rounded-sm border p-6">
+    <div class="flex items-center justify-between">
+      <p class="text-lg font-medium text-red-700">GeolocationPositionError</p>
+      <div class="h-6 w-6 animate-pulse rounded-full bg-gray-500"></div>
+    </div>
+    <p class="mt-4 text-3xl font-semibold text-red-600">
+      <slot />
+    </p>
+    <span class="text-red-400">{{
+      code === 1
+        ? `${code} - PERMISSION_DENIED`
+        : code === 2
+          ? `${code} - POSITION_UNAVAILABLE`
+          : `${code} - TIMEOUT`
+    }}</span>
+  </div>
+</template>
--- apps/weather/src/views/HomeView.vue
+++ apps/weather/src/views/HomeView.vue
@@ -1,25 +1,42 @@
 <script setup lang="ts">
 import CardSkeleton from '@/components/CardSkeleton.vue'
 import CurrentCard from '@/components/CurrentCard.vue'
+import ErrorCard from '@/components/ErrorCard.vue'
 import FutureCards from '@/components/FutureCards.vue'
+import { onErrorCaptured, ref } from 'vue'
+
+const error = ref<GeolocationPositionError>()
+
+onErrorCaptured<GeolocationPositionError>((err) => {
+  error.value = err
+})
 </script>
 
 <template>
   <main>
-    <Suspense>
-      <CurrentCard />
-      <template #fallback>
-        <CardSkeleton class="shadow-lg" />
-      </template>
-    </Suspense>
+    <ErrorCard v-if="error" :code="error.code">
+      {{ error.message }}
+    </ErrorCard>
+    <template v-else>
+      <Suspense>
+        <CurrentCard />
+        <template #fallback>
+          <CardSkeleton class="shadow-lg" />
+        </template>
+      </Suspense>
 
-    <Suspense>
-      <FutureCards />
-      <template #fallback>
-        <div class="md:grid md:grid-cols-3 md:justify-items-center">
-          <CardSkeleton v-for="i in 9" class="w-auto shadow-xl md:w-60 lg:w-80 xl:w-96" :key="i" />
-        </div>
-      </template>
-    </Suspense>
+      <Suspense>
+        <FutureCards />
+        <template #fallback>
+          <div class="md:grid md:grid-cols-3 md:justify-items-center">
+            <CardSkeleton
+              v-for="i in 9"
+              class="w-auto shadow-xl md:w-60 lg:w-80 xl:w-96"
+              :key="i"
+            />
+          </div>
+        </template>
+      </Suspense>
+    </template>
   </main>
 </template>
