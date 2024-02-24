29929a15415957b00331ef8143fe722f9d4328b3
yuwek <yuuuwek@gmail.com>
2024-02-24 07:58:35 -0300

    feat(chibi-url): add `default` layout

---
+++ apps/chibi-url/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-dvh bg-[#121212] p-4 text-white">
+    <header class="container mx-auto flex items-center justify-between">
+      <div class="flex items-center gap-2">
+        <NuxtLink to="/">
+          <IconKiwi class="size-16 fill-[#574339]" />
+        </NuxtLink>
+        <p>url</p>
+      </div>
+
+      <Auth />
+    </header>
+    <slot />
+  </div>
+</template>
