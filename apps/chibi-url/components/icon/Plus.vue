a44d37a6a21138ea284f21f7347bf8eba77d6c0a
yuwek <yuuuwek@gmail.com>
2024-02-24 07:57:59 -0300

    feat(chibi-url): add components icons

---
+++ apps/chibi-url/components/icon/Plus.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    fill="currentColor"
+    stroke-width="0"
+    class="size-5"
+  >
+    <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>
+  </svg>
+</template>
