c9db5132ae07f646d2870e671fc5700acd005c60
yuwek <yuuuwek@gmail.com>
2024-02-24 07:56:51 -0300

    pkg(chibi-url): add supabase & tailwindcss as nuxt packages

--- apps/chibi-url/nuxt.config.ts
+++ apps/chibi-url/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
+  supabase: {
+    redirectOptions: {
+      login: '/login',
+      callback: '/confirm',
+      exclude: ['/']
+    }
+  }
 })
