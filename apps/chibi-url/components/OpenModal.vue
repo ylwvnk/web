3ebeabed49a2c755ecb5f6105c0a10121d186a6e
yuwek <yuuuwek@gmail.com>
2024-03-03 02:15:15 -0300

    feat(chibi-url): add components

---
+++ apps/chibi-url/components/OpenModal.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const model = defineModel<boolean>()
+
+const openModal = () => {
+  model.value = true
+}
+</script>
+
+<template>
+  <button class="rounded bg-[#1d1d1d] p-3" @click="openModal">
+    <IconPlus class="size-5" />
+  </button>
+</template>
---
+++ apps/chibi-url/components/Stats.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { createdAt, views } = defineProps<{
+  createdAt: string
+  views: number
+}>()
+
+const timeAgo = formatRelativeTime(createdAt)
+</script>
+
+<template>
+  <div class="flex justify-between">
+    <div class="flex items-center gap-1.5 text-sm">
+      <IconEye class="size-5" />
+      <p>{{ views }}</p>
+      <span>{{ '|' }}</span>
+      <p>{{ timeAgo }}</p>
+    </div>
+
+    <slot />
+  </div>
+</template>
