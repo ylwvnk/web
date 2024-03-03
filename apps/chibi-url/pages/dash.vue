dea2a804a3eb783113bde5a2c05af5a62414c30e
yuwek <yuuuwek@gmail.com>
2024-03-03 02:04:45 -0300

    feat(chibi-url): add search functionality and create url with a modal

--- apps/chibi-url/pages/dash.vue
+++ apps/chibi-url/pages/dash.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+const showCreateModal = useState('create-modal', () => false)
+const { filtered, userUrl, pending, refresh } = await useFilter('/api/user/url')
+</script>
+
 <template>
-  <p>dashboard</p>
+  <main class="container mx-auto">
+    <div class="flex w-full items-center gap-4">
+      <Search v-model="userUrl" class="w-full" />
+      <OpenModal v-model="showCreateModal" />
+      <ModalCreate v-model="showCreateModal" @refresh="refresh" />
+    </div>
+
+    <ul class="mt-5 grid grid-cols-3 place-items-center gap-4">
+      <template v-if="pending">
+        <li v-for="n in 9" :key="n">
+          <FallbackCard>
+            <FallbackStats />
+          </FallbackCard>
+        </li>
+      </template>
+
+      <template v-else>
+        <li v-for="{ url, short, views, created_at, id } in filtered" :key="id">
+          <Card :url="url" :shortUrl="short">
+            <Stats :views="views" :createdAt="created_at">
+              <div class="space-x-2">
+                <FuncDelete :id="id" @refresh="refresh" />
+                <FuncClipboard :text="short" />
+              </div>
+            </Stats>
+          </Card>
+        </li>
+      </template>
+    </ul>
+  </main>
 </template>
