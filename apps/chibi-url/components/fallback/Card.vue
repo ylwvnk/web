8175d587221cb8f9c33dad00a8fce95540dad153
yuwek <yuuuwek@gmail.com>
2024-03-03 02:10:04 -0300

    feat(chibi-url): add fallbacks components

---
+++ apps/chibi-url/components/fallback/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-96 animate-pulse truncate rounded bg-[#1d1d1d] p-4">
+    <p class="h-6 w-full rounded bg-white"></p>
+
+    <div class="my-2 flex items-center gap-2">
+      <p class="h-6 w-44 rounded bg-[#c49076]"></p>
+      <div class="size-4 rounded bg-[#f0bd5f]"></div>
+    </div>
+
+    <slot />
+  </div>
+</template>
---
+++ apps/chibi-url/components/fallback/Stats.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="flex items-center gap-1.5 text-sm">
+      <div class="size-5 rounded bg-white" />
+      <div class="h-5 w-2 rounded bg-white" />
+      <div class="h-5 w-1 rounded bg-white" />
+      <div class="h-5 w-56 rounded bg-white" />
+    </div>
+
+    <slot />
+  </div>
+</template>
